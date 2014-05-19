5abb8d85e502d8c995a0fd3ab4ca6694644e5c63
herpoi <herpoi2006@gmail.com>
2014-05-19 13:31:50 +0200

    Revert "Powrót dvb.cpp do oryginału z OpenPLi (w większej części) - sprawdzić czy czegoś nie psuje"
    This reverts commit fb648da88d261d790f57432fd2df2ba449c21bfc.

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -392,16 +392,16 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 
 	eDebug("linking adapter%d/frontend0 to vtuner%d", nr, vtunerid);
 
-//	filter.input = DMX_IN_FRONTEND;
-//	filter.flags = 0;
-//	filter.pid = 0;
-//	filter.output = DMX_OUT_TSDEMUX_TAP;
-//	filter.pes_type = DMX_PES_OTHER;
+	filter.input = DMX_IN_FRONTEND;
+	filter.flags = 0;
+	filter.pid = 0;
+	filter.output = DMX_OUT_TSDEMUX_TAP;
+	filter.pes_type = DMX_PES_OTHER;
 
-//#define DEMUX_BUFFER_SIZE (8 * ((188 / 4) * 4096)) /* 1.5MB */
-//	ioctl(demuxFd, DMX_SET_BUFFER_SIZE, DEMUX_BUFFER_SIZE);
-//	ioctl(demuxFd, DMX_SET_PES_FILTER, &filter);
-//	ioctl(demuxFd, DMX_START); 
+#define DEMUX_BUFFER_SIZE (8 * ((188 / 4) * 4096)) /* 1.5MB */
+	ioctl(demuxFd, DMX_SET_BUFFER_SIZE, DEMUX_BUFFER_SIZE);
+	ioctl(demuxFd, DMX_SET_PES_FILTER, &filter);
+	ioctl(demuxFd, DMX_START);
 
 	switch (fe_info.type)
 	{
@@ -490,17 +490,8 @@ void *eDVBUsbAdapter::threadproc(void *arg)
 	return user->vtunerPump();
 }
 
-static bool exist_in_pidlist(unsigned short int* pidlist, unsigned short int value)
-{
-	for (int i=0; i<30; ++i)
-		if (pidlist[i] == value)
-			return true;
-	return false;
-}
-
 void *eDVBUsbAdapter::vtunerPump()
 {
-	int pidcount = 0;
 	if (vtunerFd < 0 || demuxFd < 0 || pipeFd[0] < 0) return NULL;
 
 #define MSG_PIDLIST         14
@@ -510,35 +501,6 @@ void *eDVBUsbAdapter::vtunerPump()
 		unsigned short int pidlist[30];
 		unsigned char pad[64]; /* nobody knows the much data the driver will try to copy into our struct, add some padding to be sure */
 	};
-
-#define DEMUX_BUFFER_SIZE (8 * ((188 / 4) * 4096)) /* 1.5MB */
-	ioctl(demuxFd, DMX_SET_BUFFER_SIZE, DEMUX_BUFFER_SIZE);
-
-#if DVB_API_VERSION < 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR < 5
-	/*
-	 * HACK: several stb's with older DVB API versions do not handle the
-	 * constant starting / stopping of PES filters on their vtuner interface
-	 * very well, eventually they will stop feeding any data.
-	 * In order to work around this problem, we always start a filter, making sure
-	 * 'pidcount' never drops to zero, so the filter is never stopped.
-	 *
-	 * Note that this isn't allowed for recent DVB API versions, because they
-	 * refuse to start filters while the frontend is sleeping (e.g. not tuned).
-	 */
-	{
-		struct dmx_pes_filter_params filter;
-		filter.input = DMX_IN_FRONTEND;
-		filter.flags = 0;
-		filter.pid = 0;
-		filter.output = DMX_OUT_TSDEMUX_TAP;
-		filter.pes_type = DMX_PES_OTHER;
-		if (ioctl(demuxFd, DMX_SET_PES_FILTER, &filter) >= 0
-				&& ioctl(demuxFd, DMX_START) >= 0)
-		{
-			pidcount = 1;
-		}
-	}
-#endif
 
 	while (running)
 	{
@@ -555,6 +517,7 @@ void *eDVBUsbAdapter::vtunerPump()
 		{
 			if (FD_ISSET(vtunerFd, &xset))
 			{
+				int i, j;
 				struct vtuner_message message;
 				memset(message.pidlist, 0xff, sizeof(message.pidlist));
 				::ioctl(vtunerFd, VTUNER_GET_MESSAGE, &message);
@@ -563,57 +526,48 @@ void *eDVBUsbAdapter::vtunerPump()
 				{
 				case MSG_PIDLIST:
 					/* remove old pids */
-					for (int i = 0; i < 30; i++)
+					for (i = 0; i < 30; i++)
 					{
-						if (pidList[i] == 0xffff)
-							continue;
-						if (exist_in_pidlist(message.pidlist, pidList[i]))
-							continue;
+						bool found = false;
+						if (pidList[i] == 0xffff) continue;
+						for (j = 0; j < 30; j++)
+						{
+							if (pidList[i] == message.pidlist[j])
+							{
+								found = true;
+								break;
+							}
+						}
 
-						if (pidcount > 1)
-						{
-							::ioctl(demuxFd, DMX_REMOVE_PID, &pidList[i]);
-							pidcount--;
-						}
-						else if (pidcount == 1)
-						{
-							::ioctl(demuxFd, DMX_STOP);
-							pidcount = 0;
-						}
+						if (found) continue;
+
+						::ioctl(demuxFd, DMX_REMOVE_PID, &pidList[i]);
 					}
 
 					/* add new pids */
-					for (int i = 0; i < 30; i++)
+					for (i = 0; i < 30; i++)
 					{
-						if (message.pidlist[i] == 0xffff)
-							continue;
-						if (exist_in_pidlist(pidList, message.pidlist[i]))
-							continue;
-
-						if (pidcount)
+						bool found = false;
+						if (message.pidlist[i] == 0xffff) continue;
+						for (j = 0; j < 30; j++)
 						{
-							::ioctl(demuxFd, DMX_ADD_PID, &message.pidlist[i]);
-							pidcount++;
-						}
-						else
-						{
-							struct dmx_pes_filter_params filter;
-							filter.input = DMX_IN_FRONTEND;
-							filter.flags = 0;
-							filter.pid = message.pidlist[i];
-							filter.output = DMX_OUT_TSDEMUX_TAP;
-							filter.pes_type = DMX_PES_OTHER;
-							if (ioctl(demuxFd, DMX_SET_PES_FILTER, &filter) >= 0
-									&& ioctl(demuxFd, DMX_START) >= 0)
+							if (message.pidlist[i] == pidList[j])
 							{
-								pidcount = 1;
+								found = true;
+								break;
 							}
 						}
+
+						if (found) continue;
+
+						::ioctl(demuxFd, DMX_ADD_PID, &message.pidlist[i]);
 					}
 
 					/* copy pids */
-					memcpy(pidList, message.pidlist, sizeof(message.pidlist));
-
+					for (i = 0; i < 30; i++)
+					{
+						pidList[i] = message.pidlist[i];
+					}
 					break;
 				}
 			}
@@ -669,7 +623,8 @@ void eDVBResourceManager::addAdapter(iDVBAdapter *adapter, bool front)
 			m_frontend.push_back(new_fe);
 			frontend->setSEC(m_sec);
 			// we must link all dvb-t frontends ( for active antenna voltage )
-			if (frontend->supportsDeliverySystem(SYS_DVBT, false) || frontend->supportsDeliverySystem(SYS_DVBT2, false))
+			//if (frontend->supportsDeliverySystem(SYS_DVBT, false) || frontend->supportsDeliverySystem(SYS_DVBT2, false))
+			if (frontend->supportsDeliverySystem(SYS_DVBT, false))
 			{
 				if (prev_dvbt_frontend)
 				{
@@ -692,7 +647,8 @@ void eDVBResourceManager::addAdapter(iDVBAdapter *adapter, bool front)
 			m_simulate_frontend.push_back(new_fe);
 			frontend->setSEC(m_sec);
 			// we must link all dvb-t frontends ( for active antenna voltage )
-			if (frontend->supportsDeliverySystem(SYS_DVBT, false) || frontend->supportsDeliverySystem(SYS_DVBT2, false))
+			//if (frontend->supportsDeliverySystem(SYS_DVBT, false) || frontend->supportsDeliverySystem(SYS_DVBT2, false))
+			if (frontend->supportsDeliverySystem(SYS_DVBT, false))
 			{
 				if (prev_dvbt_frontend)
 				{
@@ -838,17 +794,17 @@ bool eDVBResourceManager::frontendIsCompatible(int index, const char *type)
 			{
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBS, false);
 			}
-			else if (!strcmp(type, "DVB-T2"))
+			/*else if (!strcmp(type, "DVB-T2"))
 			{
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBT2, false);
-			}
+			}*/
 			else if (!strcmp(type, "DVB-T"))
 			{
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBT, false);
 			}
 			else if (!strcmp(type, "DVB-C"))
 			{
-#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6
+#ifdef SYS_DVBC_ANNEX_A
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_A, false) || i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_C, false);
 #else
 				return i->m_frontend->supportsDeliverySystem(SYS_DVBC_ANNEX_AC, false);
@@ -876,14 +832,15 @@ void eDVBResourceManager::setFrontendType(int index, const char *type)
 				whitelist.push_back(SYS_DVBS);
 				whitelist.push_back(SYS_DVBS2);
 			}
-			else if (!strcmp(type, "DVB-T2") || !strcmp(type, "DVB-T"))
+			//else if (!strcmp(type, "DVB-T2") || !strcmp(type, "DVB-T"))
+			else if (!strcmp(type, "DVB-T"))
 			{
 				whitelist.push_back(SYS_DVBT);
-				whitelist.push_back(SYS_DVBT2);
+				//whitelist.push_back(SYS_DVBT2);
 			}
 			else if (!strcmp(type, "DVB-C"))
 			{
-#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6
+#ifdef SYS_DVBC_ANNEX_A
 				whitelist.push_back(SYS_DVBC_ANNEX_A);
 				whitelist.push_back(SYS_DVBC_ANNEX_C);
 #else
