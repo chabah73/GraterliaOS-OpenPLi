60eebd2ff95da39caa96f7cd199698c2853a35cd
herpoi <herpoi2006@gmail.com>
2015-08-25 14:43:34 +0200

    dvb.cpp: poprawka ostatiej wstawki, coś pogmerałem w opcji wykrywania odbiornika

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -179,9 +179,13 @@ eDVBResourceManager::eDVBResourceManager()
 	else {
 		m_boxtype = BOX_SH4;
 	}
+	eDebug("[eDVBResourceManager] found %zd adapter, %zd frontends(%zd sim) and %zd demux",
+		m_adapter.size(), m_frontend.size(), m_simulate_frontend.size(), m_demux.size());
 #else
 	else {
-		eDebug("boxtype detection via /proc/stb/info not possible... use fallback via demux count!\n");
+		eDebug("[eDVBResourceManager] cannot open /proc/stb/info. Use fallback via demux count!");
+	}
+	if (m_boxtype == -1) {
 		if (m_demux.size() == 3)
 			m_boxtype = DM800;
 		else if (m_demux.size() < 5)
@@ -189,11 +193,9 @@ eDVBResourceManager::eDVBResourceManager()
 		else
 			m_boxtype = DM8000;
 	}
+
 	eDebug("[eDVBResourceManager] found %zd adapter, %zd frontends(%zd sim) and %zd demux, boxtype %d",
 		m_adapter.size(), m_frontend.size(), m_simulate_frontend.size(), m_demux.size(), m_boxtype);
-#else
-	eDebug("[eDVBResourceManager] found %zd adapter, %zd frontends(%zd sim) and %zd demux",
-		m_adapter.size(), m_frontend.size(), m_simulate_frontend.size(), m_demux.size());
 #endif
 
 	CONNECT(m_releaseCachedChannelTimer->timeout, eDVBResourceManager::releaseCachedChannel);
