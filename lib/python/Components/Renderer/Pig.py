2f0a0d8cb2aaf8ea6bab07b4f66b6ca21569954b
herpoi <herpoi2006@gmail.com>
2015-09-09 15:37:34 +0200

    Pig.py: wyłączenie PIG dla ADB2850 i ADB5800 (z wiadomych względów)

--- lib/python/Components/Renderer/Pig.py
+++ lib/python/Components/Renderer/Pig.py
@@ -5,6 +5,18 @@ from Renderer import Renderer
 from enigma import eVideoWidget, eSize, eRect, ePoint, getDesktop
 from Screens.PictureInPicture import PipPigMode
 
+PIGenabled=True
+try:
+	from Tools.GOSHardwareInfo import GOSHardwareInfo
+	if GOSHardwareInfo().get_rcstype() == 'ADB5800' or GOSHardwareInfo().get_rcstype() == 'ADB2850':
+		print 'PIG disabled'
+		PIGenabled=False
+except:
+	from Tools.HardwareInfo import HardwareInfo
+	if HardwareInfo().get_device_name() == "adb_box":
+		print 'PIG disabled'
+		PIGenabled=False
+  
 class Pig(Renderer):
 	def __init__(self):
 		Renderer.__init__(self)
@@ -19,21 +31,24 @@ class Pig(Renderer):
 		instance.setFBSize(desk.size())
 
 	def applySkin(self, desktop, parent):
-		attribs = self.skinAttributes[:]
-		for (attrib, value) in self.skinAttributes:
-			if attrib == "hidePip":
-				self.hidePip = value == 1
-				attribs.remove((attrib,value))
-				break
-		self.skinAttributes = attribs
-		ret = Renderer.applySkin(self, desktop, parent)
-		if ret:
-			self.Position = self.instance.position()
-			self.Size = self.instance.size()
-		return ret
+		if PIGenabled:
+			attribs = self.skinAttributes[:]
+			for (attrib, value) in self.skinAttributes:
+				if attrib == "hidePip":
+					self.hidePip = value == 1
+					attribs.remove((attrib,value))
+					break
+			self.skinAttributes = attribs
+			ret = Renderer.applySkin(self, desktop, parent)
+			if ret:
+				self.Position = self.instance.position()
+				self.Size = self.instance.size()
+			return ret
+		else:
+			return 0
 
 	def onShow(self):
-		if self.instance:
+		if self.instance and PIGenabled:
 			if self.Size:
 				self.instance.resize(self.Size)
 			if self.Position:
@@ -41,6 +56,6 @@ class Pig(Renderer):
 			self.hidePip and PipPigMode(True)
 
 	def onHide(self):
-		if self.instance:
+		if self.instance and PIGenabled:
 			self.preWidgetRemove(self.instance)
 			self.hidePip and PipPigMode(False)
